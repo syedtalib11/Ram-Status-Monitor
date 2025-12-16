8301af64f8630d2f8751f98634c7ac43da3f2e42
Syed Talib <talibhassann@gmail.com>
2025-12-17 00:38:06 +0300

    Update Disk-Space-Alert.sh

--- Disk-Space-Alert.sh
+++ Disk-Space-Alert.sh
@@ -5,7 +5,7 @@
 D_use=$(df -hT | egrep -vi "filesystem|tmpfs" | grep "sda1" | awk '{print $6}' | tr -d %)
 FU=80
 TO="syedtalib98634@gmail.com"
-if [[ "$D_use" -ge "$TH" ]]
+if [[ "$D_use" -ge "$FU" ]]
 then
      echo "WARINING Disk is LOW - $FU %" | mail -s "Disk Space Alert !" $TO
 else
