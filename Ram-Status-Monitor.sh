1799304392c3f451f1c5259a73a4e37fe1a2f31d
syedtalib11 <talibhassann@gmail.com>
2025-12-16 10:23:54 +0300

    Add files via upload

---
+++ Ram-Status-Monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Disk Usage Alert
+#Author - Syed Talib
+
+Total=$(free -mt | grep "Total" | awk '{print $4}')
+TH=500
+if [[ "$Total" -le "$TH" ]]
+then
+     echo "Warning RAM is low"
+else
+     echo "your space is sufficent: $Total:M"
+fi
